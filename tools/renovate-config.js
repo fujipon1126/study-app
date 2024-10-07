5ea74c4e72a5eae3404073316b8175c6c792fb00
fujipon1126 <fujitansp@gmail.com>
2024-10-07 11:57:48 +0900

    renovate導入

---
+++ tools/renovate-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branchPrefix: 'renovate/',
+  dryRun: null,
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  platform: 'github',
+  repositories: [
+    'fujipon1126/study-app'
+  ],
+  labels: ["dependencies"],
+  packageRules: [
+    {
+      // major,minor,patch以外のバージョンアップは無効
+      matchUpdateTypes: [ 'pin', 'pinDigest', 'digest', 'lockFileMaintenance', 'rollback', 'bump'],
+      enabled: false
+    }
+  ],
+  extends: [
+    "config:base",
+    ":dependencyDashboard",
+    ":prHourlyLimit4",
+    ":prConcurrentLimit20"
+  ]
+};
