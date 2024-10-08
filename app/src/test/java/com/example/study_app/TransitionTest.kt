1a75ff8051afb454a3557792232604c5379a402e
fujipon1126 <fujitansp@gmail.com>
2024-10-08 10:57:17 +0900

    他の遷移系のテストも書いた

--- app/src/test/java/com/example/study_app/TransitionTest.kt
+++ app/src/test/java/com/example/study_app/TransitionTest.kt
@@ -3,9 +3,7 @@ package com.example.study_app
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.printToLog
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.study_app.composable.MainComposable
 import com.google.common.truth.Truth
@@ -29,12 +27,64 @@ class TransitionTest {
             )
         }
 
-        rule.onRoot().printToLog("test")
-
         rule.onNodeWithText("Navigate RequestPermissionComposable")
             .assertIsDisplayed()
             .performClick()
 
         Truth.assertThat(expectString).isEqualTo("NavigateToRequest")
+    }
+
+    @Test
+    fun `PhotoPickerComposable遷移テスト`() {
+        var expectString = ""
+        rule.setContent {
+            MainComposable(
+                onNavigateToPhotoPicker = {
+                    expectString = "NavigateToPhotoPicker"
+                }
+            )
+        }
+
+        rule.onNodeWithText("Navigate PhotoPickerComposable")
+            .assertIsDisplayed()
+            .performClick()
+
+        Truth.assertThat(expectString).isEqualTo("NavigateToPhotoPicker")
+    }
+
+    @Test
+    fun `ZoomImageComposable遷移テスト`() {
+        var expectString = ""
+        rule.setContent {
+            MainComposable(
+                onNavigateToZoomImage = {
+                    expectString = "NavigateToZoomImage"
+                }
+            )
+        }
+
+        rule.onNodeWithText("Navigate ZoomImageComposable")
+            .assertIsDisplayed()
+            .performClick()
+
+        Truth.assertThat(expectString).isEqualTo("NavigateToZoomImage")
+    }
+
+    @Test
+    fun `QiitaListScreen遷移テスト`() {
+        var expectString = ""
+        rule.setContent {
+            MainComposable(
+                onQiitaApi = {
+                    expectString = "QiitaApi"
+                }
+            )
+        }
+
+        rule.onNodeWithText("Qiita Api Test Composable")
+            .assertIsDisplayed()
+            .performClick()
+
+        Truth.assertThat(expectString).isEqualTo("QiitaApi")
     }
 }
